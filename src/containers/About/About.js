e20046f0e3ddcdab2769ce09b4b34df3b817d5fc
Brandon <blenz@expapp.com>
2020-04-13 20:34:24 -0400

    Minor restyle of About section

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -3,12 +3,14 @@ import React from 'react';
 import classes from './About.module.css'
 
 import Headshot from '../../components/Media/Headshot/Headshot';
+import InlineLink from '../../components/UI/InlineLink/InlineLink';
 
 const About = () => (
     <div className={classes.About}>
         <div className={classes.FloatingHeadshot}>
             <Headshot />
         </div>
+        <div className={classes.HelloHeader}>Hi, I'm Brandon!</div>
         <p>
             Hello and welcome to my website! I'm an engineer with a passion for code and food. I love to optimize both
             form and function of whatever I'm currently working on. When cooking I want my food to look and taste
@@ -16,8 +18,8 @@ const About = () => (
         </p>
         <p>
             Think of this page as a creative portfolio where I have the opportunity
-            to showcase some of the technologies I've learned. For a full professional summary feel free to head over
-            to my <a href="https://www.linkedin.com/in/lenzbrandon/">LinkedIn</a>.
+            to showcase some of the technologies I've learned. For a full professional summary and resume feel free to head over
+            to my <InlineLink link="https://www.linkedin.com/in/lenzbrandon/">LinkedIn</InlineLink>.
         </p>
     </div>
 );
