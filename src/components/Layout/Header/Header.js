d78f9cf2934697fa0e44e79f6345324be5315877
Brandon <blenz@expapp.com>
2020-05-20 21:53:21 -0400

    Remove unused navigation

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -4,22 +4,21 @@ import { MdExpandMore } from 'react-icons/md';
 import classes from './Header.module.css'
 import background from '../../../assets/images/atlanta_skyline_piedmont_park_christopher_alvarenga_smaller.jpg'
 
-import Navigation from '../../Navigation/Navigation';
-
 const Header = ({ onScrollToNext }) => (
-    <header
+    <>
+        <header
         className={classes.Header}
         style={{
             backgroundImage: `linear-gradient(rgba(0,0,0,0.25) 0%, rgba(0,0,0,0.75) 100%), url(${background})`
         }}>
-        <div className={classes.HeaderText}>
-            <div className={classes.FirstName}>Brandon</div>
-            <div className={classes.LastName}>Lenz</div>
-            <div className={classes.Title}>Engineer</div>
-        </div>
-        {/*<Navigation /> //TODO: Delete if not using navigation*/}
-        <MdExpandMore className={classes.Caret} onClick={onScrollToNext}/>
-    </header>
+            <div className={classes.HeaderText}>
+                <div className={classes.FirstName}>Brandon</div>
+                <div className={classes.LastName}>Lenz</div>
+                <div className={classes.Title}>Engineer</div>
+            </div>
+            <MdExpandMore className={classes.Caret} onClick={onScrollToNext}/>
+        </header>
+    </>
 );
 
 export default Header;
