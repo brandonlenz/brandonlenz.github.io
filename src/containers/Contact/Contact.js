b4913187de583e0c05c22234b9febff839d6c3a6
Brandon <blenz@expapp.com>
2020-05-21 10:04:22 -0400

    Basic skeleton and preliminary styling for Contact section

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,28 +1,24 @@
 import React from 'react';
+import { FaRegEnvelope, FaLinkedinIn } from 'react-icons/fa'
 
 import classes from './Contact.module.css'
 
 const Contact = () => (
-    //TODO: Change all of this into an email form
     <div className={classes.Contact}>
-        <h3>Want to get in contact?</h3>
-        <p>
-            Please feel free to reach out to me through any of the provided avenues.
-            If I'm not immediately available I'll return calls, emails, and messages promptly.
-        </p>
+        <div className={classes.ContactHeader}>Want to get in contact?</div>
 
-        <h3>Contact Details</h3>
-        <ul>
-            <li>
-                <a href="tel:404-323-3155">404-323-3155</a>
-            </li>
-            <li>
-                <a href="mailto:brandonalenz@gmail.com">brandonalenz@gmail.com</a>
-            </li>
-            <li>
-                <a href="http://linkedin.com/in/lenzbrandon">Contact Brandon on LinkedIn</a>
-            </li>
-        </ul>
+        <div className={classes.ContactMethods}>
+            <div className={classes.ContactMethod}>
+                <a href="mailto:brandonalenz@gmail.com">
+                    <FaRegEnvelope className={classes.Icon}/>
+                </a>
+            </div>
+            <div className={classes.ContactMethod}>
+                <a href="http://linkedin.com/in/lenzbrandon">
+                    <FaLinkedinIn className={classes.Icon}/>
+                </a>
+            </div>
+        </div>
     </div>
 );
 
