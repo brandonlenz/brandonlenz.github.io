b94916cad1db2d2ebff610763567541c834c59e7
Brandon <blenz@expapp.com>
2020-07-06 23:08:05 -0400

    Updates

---
+++ precache-manifest.9a723ff1bdf86e54dda218483013f54a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323a9d40aaec8584fb47cc1d76e70c7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ab77aae530f07161393",
+    "url": "/static/css/2.d9ee7e2f.chunk.css"
+  },
+  {
+    "revision": "25a1e296302788305e81",
+    "url": "/static/css/main.0dbfc30d.chunk.css"
+  },
+  {
+    "revision": "8ab77aae530f07161393",
+    "url": "/static/js/2.5e85b71f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5e85b71f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25a1e296302788305e81",
+    "url": "/static/js/main.0fc4f79a.chunk.js"
+  },
+  {
+    "revision": "eb718d74623af6cd4202",
+    "url": "/static/js/runtime-main.4476f73d.js"
+  },
+  {
+    "revision": "e55b9244c6cddcddf91d7fee19bba858",
+    "url": "/static/media/Headshot_300.e55b9244.png"
+  },
+  {
+    "revision": "3398e9a1b71e92148479437b24b3735a",
+    "url": "/static/media/atlanta_skyline_piedmont_park_christopher_alvarenga_smaller.3398e9a1.jpg"
+  }
+]);
--- src/components/UI/InlineLink/InlineLink.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import classes from './InlineLink.module.css'
-
-const InlineLink = ({ url, text }) => (
-    <a className={classes.InlineLink} href={url} target="_blank">{text}</a>
-);
-
-export default InlineLink;
