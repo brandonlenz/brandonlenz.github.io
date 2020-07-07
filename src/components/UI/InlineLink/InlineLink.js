1bf7d32c6acd2e2abeb3730f9b7f29dd658f3bf9
Brandon <blenz@expapp.com>
2020-07-07 18:56:06 -0400

    Fix cross scripting vulnerability

--- src/components/UI/InlineLink/InlineLink.js
+++ src/components/UI/InlineLink/InlineLink.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './InlineLink.module.css'
 
 const InlineLink = ({ url, text }) => (
-    <a className={classes.InlineLink} href={url} target="_blank">{text}</a>
+    <a className={classes.InlineLink} href={url} target="_blank" rel="noreferrer noopener">{text}</a>
 );
 
 export default InlineLink;
