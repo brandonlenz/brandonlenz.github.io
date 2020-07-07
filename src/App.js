5e2c5237e28634baecdeadf705620736c3a3b607
Brandon <blenz@expapp.com>
2020-07-06 20:24:14 -0400

    Update About section to animate on scroll

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
+import About from './containers/About/About';
+import Contact from './containers/Contact/Contact';
 import Layout from './containers/Layout/Layout';
 import Portfolio from './containers/Portfolio/Portfolio';
-import Contact from './containers/Contact/Contact';
-import About from './containers/About/About';
 
 function App() {
   return (
--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,26 +1,37 @@
 import React from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
 
-import classes from './About.module.css'
+import classes from './About.module.css';
 
 import Headshot from '../../components/Media/Headshot/Headshot';
 import InlineLink from '../../components/UI/InlineLink/InlineLink';
 
 const About = () => (
     <div className={classes.About}>
-        <div className={classes.FloatingHeadshot}>
-            <Headshot />
-        </div>
-        <div className={classes.HelloHeader}>Hi, I'm Brandon!</div>
-        <p>
-            I'm an engineer with a passion for code and food. I love to optimize both
-            form and function of whatever I'm currently working on. When cooking I want my food to look and taste
-            delicious. Likewise, when coding I want my work's aesthetic and functionality to go hand in hand.
-        </p>
-        <p>
-            Think of this page as a creative portfolio where I have the opportunity
-            to showcase some of the technologies I've learned. For a full professional summary and resume feel free to head over
-            to my <InlineLink text="LinkedIn" url="https://www.linkedin.com/in/lenzbrandon/" />.
-        </p>
+        <ScrollAnimation animateIn='fadeInDown'>
+            <div className={classes.FloatingHeadshot}>
+                <Headshot />
+            </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn='fadeInUp'>
+            <div className={classes.HelloHeader}>Hi, I'm Brandon!</div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn='fadeInLeft'>
+            <p>
+                I'm an engineer with a passion for code and food. I love to optimize both
+                form and function of whatever I'm currently working on. When cooking I want my food to look and taste
+                delicious. Likewise, when coding I want my work's aesthetic and functionality to go hand in hand.
+            </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn='fadeInRight'>
+            <p>
+                Think of this page as a creative portfolio where I have the opportunity
+                to showcase some of the technologies I've learned. For a full professional summary and resume feel free to head over
+                to my <InlineLink text="LinkedIn" url="https://www.linkedin.com/in/lenzbrandon/" />.
+            </p>
+        </ScrollAnimation>
     </div>
 );
 
